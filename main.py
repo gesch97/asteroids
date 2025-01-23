39b3630ff90e15de97fb56d985c0ff1c90636a73
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-01-23 13:49:38 +0100

    player can move now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from player import Player
 
 
 def main():
@@ -9,11 +10,19 @@ def main():
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    clk = pygame.time.Clock()
+    dt = 0
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+        # drawing the screen
         screen.fill((100, 0, 0))
+        player.draw(screen)
+
+        dt = clk.tick(60) / 1000
         pygame.display.flip()
 
 
