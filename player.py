a565d819996233b556863975e4eb20c9e94c8160
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-01-24 13:11:54 +0100

    asteroids added, player can shoot now

--- player.py
+++ player.py
@@ -1,6 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    SHOT_RADIUS,
+)
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -31,7 +38,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        new_shot.velocity = (
+            pygame.Vector2((0, 1)).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        )
