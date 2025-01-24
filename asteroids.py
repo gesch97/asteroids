879dc2b0c6d5f0499e80155b28f8b6572ee2182d
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-01-24 13:38:22 +0100

    shots can collide now

--- asteroids.py
+++ asteroids.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
 class Asteroids(CircleShape):
@@ -11,3 +13,20 @@ class Asteroids(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if ASTEROID_MIN_RADIUS >= self.radius:
+            return
+
+        angle = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new_velocity_a = self.velocity.rotate(angle)
+        new_velocity_b = self.velocity.rotate(-angle)
+
+        new_asteroid_a = Asteroids(self.position.x, self.position.y, new_radius)
+        new_asteroid_b = Asteroids(self.position.x, self.position.y, new_radius)
+
+        new_asteroid_a.velocity = new_velocity_a * 1.2
+        new_asteroid_b.velocity = new_velocity_b * 1.2
